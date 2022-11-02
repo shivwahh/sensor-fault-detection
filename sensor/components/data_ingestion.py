e38599e6fe364eabfd22db86dc65b3c5f5ecf990
shivwahh <shivarajbevoor1996@gmail.com>
2022-11-02 19:27:12 +0530

    added and updated main_utils.py

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -6,5 +6,11 @@ from sklearn.model_selection import train_test_split
 
 from sensor.constant.training_pipeline import SCHEMA_DROP_COLS, SCHEMA_FILE_PATH
 from sensor.data_access.sensor_data import SensorData
-from sensor.entity.artifact_entity import DataInge
+from sensor.entity.artifact_entity import DataIngestionArtifact
+from sensor.exception import SensorException
+from sensor.logger import logging
+from sensor.utils.main_utils import read_yaml_file
+
+
+
 
