65fee836a11cf963b177b8d6ea4a12672e192078
shivwahh <shivarajsb1996@gmail.com>
2022-11-02 22:32:13 +0530

    edited data ingestion related files

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -7,9 +7,12 @@ from sklearn.model_selection import train_test_split
 from sensor.constant.training_pipeline import SCHEMA_DROP_COLS, SCHEMA_FILE_PATH
 from sensor.data_access.sensor_data import SensorData
 from sensor.entity.artifact_entity import DataIngestionArtifact
+from sensor.entity.config_entity import DataIngestionConfig
 from sensor.exception import SensorException
 from sensor.logger import logging
 from sensor.utils.main_utils import read_yaml_file
+
+
 
 
 
--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -1,7 +1,94 @@
-from datetime import datetime
 import os
-from sensor.constant.training_pipeline import PIPELINE
+from dataclasses import dataclass
+from datetime import datetime
 
+
+from sensor.constant import prediction_pipeline
+from sensor.constant.training_pipeline import *
+from sensor.entity.artifact_entity import ClassificationMetricArtifact
+
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+@dataclass
 class TrainingPipelineConfig:
-    def __init__(self,timestamp=datetime.now()):
-        timestamp = timestamp.strftime
+    pipeline_name: str = PIPELINE_NAME
+
+    artifact_dir: str = os.path.join(ARTIFACT_DIR,TIMESTAMP)
+
+    timestamp: str = TIMESTAMP
+
+training_pipeline_config: TrainingPipelineConfig = TrainingPipelineConfig()
+
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(
+        training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME
+    )
+
+    feature_store_file_path: str = os.path.join(
+        data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME
+    )
+
+    training_file_path: str = os.path.join(
+        data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME
+    )
+
+    testing_file_path: str = os.path.join(
+        data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME
+    )
+
+    train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
+
+    collection_name: str = DATA_INGESTION_COLLECTION_NAME
+
+@dataclass
+class DataValidationConfig:
+    data_validation_dir: str = os.path.join(
+        training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME
+    )
+
+    valid_data_dir: str = os.path.join(data_validation_dir, DATA_VALIDATION_VALID_DIR)
+
+    invalid_data_dir: str = os.path.join(
+        data_validation_dir, DATA_VALIDATION_INVALID_DIR
+    )
+
+    valid_train_file_path: str = os.path.join(valid_data_dir, TRAIN_FILE_NAME)
+
+    valid_test_file_path: str = os.path.join(valid_data_dir, TEST_FILE_NAME)
+
+    invalid_train_file_path: str = os.path.join(invalid_data_dir, TRAIN_FILE_NAME)
+
+    invalid_test_file_path: str = os.path.join(invalid_data_dir, TEST_FILE_NAME)
+
+    drift_report_file_path: str = os.path.join(
+        data_validation_dir,
+        DATA_VALIDATION_DRIFT_REPORT_DIR,
+        DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
+    )
+
+
+@dataclass
+class DataTransformationConfig:
+    data_transformation_dir: str = os.path.join(
+        training_pipeline_config.artifact_dir, DATA_TRANSFORMATION_DIR_NAME
+    )
+
+    transformed_train_file_path: str = os.path.join(
+        data_transformation_dir,
+        DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+        TRAIN_FILE_NAME.replace("csv", "npy"),
+    )
+
+    transformed_test_file_path: str = os.path.join(
+        data_transformation_dir,
+        DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+        TEST_FILE_NAME.replace("csv", "npy"),
+    )
+
+    transformed_object_file_path: str = os.path.join(
+        data_transformation_dir,
+        DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+        PREPROCSSING_OBJECT_FILE_NAME,
+    )
+
